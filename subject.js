914ce1f58d647e893abfbbb29eaf2598a8444f6c
XYZ321COD <michalznalezniak2@gmail.com>
2020-02-28 00:21:58 +0100

    Added documentation

--- subject.js
+++ subject.js
@@ -1,10 +1,16 @@
-
+/**
+ * Loads data from Subjects.js, and adds event to every subject-box.
+ */
 function loadSubjects() {
 
     const subject_items = document.querySelectorAll('.subject');
-    //Functionality for draggable subjects
     subject_items.forEach(item => {
         item.addEventListener('dragstart', function () {
+            /**
+             * @var draggedItem
+             * @description One of the subject-boxes that we are currently dragging.
+             * @type {Object}
+             */
             draggedItem = item;
             setTimeout(function () {
                 item.style.display = 'none';
